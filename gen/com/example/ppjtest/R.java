0e241ce299a4d3d44400318c8bf0a5cd275d3785
Andrzej Kowalczuk <tieorange@gmail.com>
2014-01-09 12:42:31 +0100

    add relative views

--- gen/com/example/ppjtest/R.java
+++ gen/com/example/ppjtest/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080012;
+        public static final int btnCencel=0x7f08000b;
         public static final int btnLinear=0x7f080000;
+        public static final int btnOK=0x7f08000a;
         public static final int btnRelative=0x7f080002;
         public static final int btnTable=0x7f080001;
         public static final int button1=0x7f080003;
@@ -33,12 +35,14 @@ public final class R {
         public static final int button3=0x7f080005;
         public static final int button4=0x7f080006;
         public static final int button5=0x7f080007;
-        public static final int tableRow1=0x7f080008;
-        public static final int tableRow2=0x7f080009;
-        public static final int tableRow3=0x7f08000b;
-        public static final int textView1=0x7f08000a;
-        public static final int textView2=0x7f08000c;
-        public static final int textView3=0x7f08000d;
+        public static final int editText=0x7f080009;
+        public static final int label=0x7f080008;
+        public static final int tableRow1=0x7f08000c;
+        public static final int tableRow2=0x7f08000d;
+        public static final int tableRow3=0x7f08000f;
+        public static final int textView1=0x7f08000e;
+        public static final int textView2=0x7f080010;
+        public static final int textView3=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -50,13 +54,17 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int accept_all=0x7f05000a;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button=0x7f050004;
+        public static final int cencel=0x7f050009;
         public static final int hello_world=0x7f050002;
         public static final int linear_layout=0x7f050003;
+        public static final int ok=0x7f050008;
         public static final int relative_layout=0x7f050006;
         public static final int table_layout=0x7f050005;
+        public static final int type_here_=0x7f050007;
     }
     public static final class style {
         /** 
