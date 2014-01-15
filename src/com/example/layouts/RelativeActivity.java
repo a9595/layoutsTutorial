2ec234d3269f23560d9fb8a472d0da904d8396b5
Andrzej Kowalczuk <tieorange@gmail.com>
2014-01-15 09:12:26 +0100

    add ormLite

--- src/com/example/layouts/RelativeActivity.java
+++ src/com/example/layouts/RelativeActivity.java
@@ -2,15 +2,33 @@ package com.example.layouts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.ppjtest.R;
 
 public class RelativeActivity extends Activity {
 
+	ListView lvColors;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.relative);
+
+		
+		createListView();
+	}
+
+	void createListView() {
+		String[] myColors = { "Red", "Green", "Blue" };
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.relative, myColors);
+		
+		lvColors = (ListView) findViewById(R.id.listViewColors);
+		lvColors.setAdapter(adapter);
+
 	}
 
 }
