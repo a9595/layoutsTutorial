e05965bf356388c2245c90a0d646783fabe6cfa6
Andrzej Kowalczuk <tieorange@gmail.com>
2014-01-18 17:12:57 +0100

    show subjects

---
+++ src/com/example/layouts/Student.java
@@ -0,0 +1,22 @@
+package com.example.layouts;
+
+public class Student {
+	public int Id;
+	public String Name;
+	public String Surname;
+
+	public Student() {
+		Id = -1;
+		Name = null;
+		Surname = null;
+	}
+	
+	public Student(int id, String name, String surname) {
+		super();
+		Id = id;
+		Name = name;
+		Surname = surname;
+	}
+
+	
+}
